aa27ff0eaa2ee621e421100acf7888b7210fe6ae
abi <abi@XPS15>
2018-05-30 12:58:06 +0200

    Element base

--- Lorann/model/src/main/java/model/element/Sprite.java
+++ Lorann/model/src/main/java/model/element/Sprite.java
@@ -1,5 +1,79 @@
 package model.element;
 
+import java.awt.Image;
+
 public class Sprite {
+	private boolean loaded;
+	private String imageLink;
+	private Image image;
+	private char consoleImage;
+	private boolean visible;
+	private Direction direction;
+	private Position position;
+	private Dimension dimension;
+	private Penetrability penetrability;
+	
+	
+	public Sprite(Direction direction, Position position, Penetrability penetrability, Dimension dimension, String imageLink) {
+		
+	}
+	
+	
+	public Direction getDirection() {
+		return direction;
+	}
+	
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+	
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+	
+	public Dimension getDimension() {
+		return dimension;
+	}
+	
+	public void setDimension(Dimension dimension) {
+		this.dimension = dimension;
+	}
+	
+	public Penetrability getPenetrability() {
+		return penetrability;
+	}
+
+	public void setPenetrability(Penetrability penetrability) {
+		this.penetrability = penetrability;
+	}
+	
+	public char getConsoleImage() {
+		return consoleImage;
+	}
+	
+	public void setConsoleImage(char consoleImage) {
+		this.consoleImage = consoleImage;
+	}
+	public boolean isVisible() {
+		return visible;
+	}
+	
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	
 
 }
