b9cb7498d2eeca9b490ccb7c4edf1e47b7a69025
yoann-tillet <33153818+yoann-tillet@users.noreply.github.com>
2018-06-01 10:39:02 +0200

    map

--- Lorann/model/src/main/java/model/Map.java
+++ Lorann/model/src/main/java/model/Map.java
@@ -1,9 +1,9 @@
 package model;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.Observable;
 
 import model.element.Element;
@@ -12,77 +12,131 @@ import model.element.nonMobile.NonMobileFactory;;
 
 class Map extends Observable {
 
-    private int width;
 
-    
-    private int height;
+	private final int WIDTH = 20;
+	private final int HEIGHT = 12;
+	private Element[][] onTheMap = new Element[WIDTH][HEIGHT];
+ 
+    private static String sql  = "{call print_decor}";
 
-    private Element[][] onTheMap;
-
-
-    Map(final String fileName) throws IOException {
-        super();
-        this.loadFile(fileName);
-    }
-
-    private void loadFile(final String fileName) throws IOException {
-        final BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-        String line;
-        int y = 0;
-        line = buffer.readLine();
-        this.setWidth(Integer.parseInt(line));
-        line = buffer.readLine();
-        this.setHeight(Integer.parseInt(line));
-        this.onTheMap = new Element[this.getWidth()][this.getHeight()];
-        line = buffer.readLine();
-        while (line != null) {
-            for (int x = 0; x < line.toCharArray().length; x++) {
-                this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol(line.toCharArray()[x]), x, y);
-            }
-            line = buffer.readLine();
-            y++;
-        }
-        buffer.close();
-    }
-
-  
-  
-    public final int getWidth() {
-        return this.width;
-    }
-
-    private void setWidth(final int width) {
-        this.width = width;
-    }
+   
 
 
-    public final int getHeight() {
-        return this.height;
-    }
-
-    
-    private void setHeight(final int height) {
-        this.height = height;
+    Map() {
+        super();      
     }
 
 
 
-    public final Element getOnTheMapXY(final int x, final int y) {
-        return this.onTheMap[x][y];
-    }
 
-
-    private void setOnTheMapXY(final Element element, final int x, final int y) {
-        this.onTheMap[x][y] = element;
+    public void semer() {
+    		
+    	
+    		
+    		try {
+    			Connection cnx = model.dao.LorannBDDConnector.connecterDB();			
+    			CallableStatement  cs;
+    			ResultSet rst;
+    			
+    			
+    			cs = cnx.prepareCall(sql);
+    	        if (cs.execute()) {
+    	            rst = cs.getResultSet();
+    	            
+    	            while(rst.next()) {
+    	            	int levelNumber = 0;
+    	            	int levelNumberBdd = rst.getInt("level_number");
+    	            	String type = rst.getString("type");
+    	    			int x = rst.getInt("x");
+    	    			int y = rst.getInt("y");
+    	    			
+    	    			if(levelNumber == levelNumberBdd) {
+    		    			switch(type) {
+    		    			case "HWall" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('H'), x, y);  
+    		    				break;
+    		    			case "VWall" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('V'), x, y);  
+    		    				break;
+    		    			case "CWall" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('C'), x, y);   
+    		    				break;
+    		    			case "Lorann" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('L'), x, y);  
+    		    				break;
+    		    			case "Door" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('D'), x, y);  
+    		    				break;
+    		    			case "Bubble" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('B'), x, y);  
+    		    				break;
+    		    			case "Monster_1" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('Y'), x, y);  
+    		    				break;
+    		    			case "Monster_2" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('U'), x, y);   
+    		    				break;
+    		    			case "Monster_3" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('I'), x, y);  
+    		    				break;
+    		    			case "Monster_4" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('O'), x, y);    
+    		    				break;
+    		    			case "Purse" :
+    		    				this.setOnTheMapXY(NonMobileFactory.getFromFileSymbol('P'), x, y);   
+    		    				break;
+    		    			}
+    		            }
+    	            }
+    	            rst.close();
+    	        }
+    			
+    		
+    			
+    		} catch (Exception ex) {
+    			ex.printStackTrace();
+    		}
+    		
+    	}
+        
+        
+        
+    	public final int getWidth() {
+    	    return this.WIDTH;
+    	}
+    	
+    	/*private void setWidth(final int width) {
+    	    this.WIDTH = width;
+    	}*/
+    	
+    	
+    	public final int getHeight() {
+    	    return this.HEIGHT;
+    	}
+    	
+    	
+    	/*private void setHeight(final int height) {
+    	    this.HEIGHT = height;
+    	}*/
+    	
+    	
+    	
+    	public final Element getOnTheMapXY(final int x, final int y) {
+    	    return this.onTheMap[x][y];
+    	}
+    	
+    	
+    	private void setOnTheMapXY(final Element element, final int x, final int y) {
+    	    this.onTheMap[x][y] = element;
+    	}
+    	
+    	
+    	/*public final void setMobileHasChanged() {
+    	    this.setChanged();
+    	    this.notifyObservers();
+    	}
+    	
+    	public Observable getObservable() {
+    	    return this;
+    	}*/
     }
-
-  
-    public final void setMobileHasChanged() {
-        this.setChanged();
-        this.notifyObservers();
-    }
-
-    public Observable getObservable() {
-        return this;
-    }
-}
