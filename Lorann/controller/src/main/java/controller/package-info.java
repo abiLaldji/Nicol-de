41cd56e675b97fbaebef2d460903039fd9e43870
yoann-tillet <33153818+yoann-tillet@users.noreply.github.com>
2018-06-04 15:34:01 +0200

    author

--- Lorann/controller/src/main/java/controller/package-info.java
+++ Lorann/controller/src/main/java/controller/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Provides all classes for the controller component.
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @author Abigael LALDJI / Nathan VIVES / Jérémy DUSSAUX / Yoann TILLET
  * @version 1.0
  */
 package controller;
